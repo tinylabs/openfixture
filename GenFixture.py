a31938eb4753106752d914ee53e4a11d86ad852e
Stephan Hültl <sh3944@bingo-ev.de>
2020-05-08 23:07:37 +0200

    added parameter for DXF plot export so it will be converted to mm

--- GenFixture.py
+++ GenFixture.py
@@ -27,9 +27,9 @@ DEFAULT_PCB_TH = 1.6
 DEFAULT_SCREW_D = 3.0
 DEFAULT_SCREW_LEN = 14
 
+
 # Generate fixture class
 class GenFixture:
-
     # Layers
     layer = F_Cu
     paste = F_Paste
@@ -54,24 +54,24 @@ class GenFixture:
 
     # Optional arguments
     rev = None
-    washer_th=None
-    nut_f2f=None
-    nut_c2c=None
-    nut_th=None
-    pivot_d=None
-    border=None
+    washer_th = None
+    nut_f2f = None
+    nut_c2c = None
+    nut_th = None
+    pivot_d = None
+    border = None
 
     # Board dimensions
-    min_y = float ("inf")
-    origin = [ float ("inf"), float ("inf") ]
+    min_y = float("inf")
+    origin = [float("inf"), float("inf")]
     dims = [0, 0]
     test_points = []
-    
+
     def __init__(self, prj_name, brd, mat_th):
         self.prj_name = prj_name
         self.brd = brd
-        self.mat_th = float (mat_th)
-        
+        self.mat_th = float(mat_th)
+
     def __exit__(self, type, value, traceback):
         pass
 
@@ -94,11 +94,11 @@ class GenFixture:
 
     def SetParams(self, pcb_th, screw_len, screw_d):
         if pcb_th is not None:
-            self.pcb_th = float (pcb_th)
+            self.pcb_th = float(pcb_th)
         if screw_len is not None:
-            self.screw_len = float (screw_len)
+            self.screw_len = float(screw_len)
         if screw_d is not None:
-            self.screw_d = float (screw_d)
+            self.screw_d = float(screw_d)
 
     def SetLayers(self, layer=-1, ilayer=-1, flayer=-1):
         if layer != -1:
@@ -116,16 +116,16 @@ class GenFixture:
             self.mirror = True
 
     def Round(self, x, base=0.01):
-        return round(base*round(x/base), 2)
+        return round(base * round(x / base), 2)
 
     def PlotDXF(self, path):
 
         # Save auxillary origin
-        aux_origin_save = self.brd.GetAuxOrigin ()
+        aux_origin_save = self.brd.GetAuxOrigin()
 
         # Set new aux origin to upper left side of board
-        self.brd.SetAuxOrigin (wxPoint (FromMM(self.origin[0]), FromMM(self.origin[1])))
-        
+        self.brd.SetAuxOrigin(wxPoint(FromMM(self.origin[0]), FromMM(self.origin[1])))
+
         # Get pointers to controllers
         pctl = PLOT_CONTROLLER(self.brd)
         popt = pctl.GetPlotOptions()
@@ -134,22 +134,23 @@ class GenFixture:
         popt.SetOutputDirectory(path)
 
         # Set some important plot options:
+        popt.SetDXFPlotUnits(DXF_PLOTTER.DXF_UNIT_MILLIMETERS)
         popt.SetPlotFrameRef(False)
         popt.SetLineWidth(FromMM(0.1))
         popt.SetAutoScale(False)
         popt.SetScale(1)
         popt.SetMirror(self.mirror)
         popt.SetUseGerberAttributes(False)
-        popt.SetExcludeEdgeLayer(False);
+        popt.SetExcludeEdgeLayer(False)
 
         # Use auxillary origin
-        popt.SetUseAuxOrigin(True)
+        popt.SetUseAuxOrigin(False)
 
         # This by gerbers only (also the name is truly horrid!)
         popt.SetSubtractMaskFromSilk(False)
 
         # Do the BRD edges in black
-        popt.SetColor(COLOR4D(0,0,0,1.0)) # color4d = RED, GREEN, BLUE, OPACITY
+        popt.SetColor(COLOR4D(0, 0, 0, 1.0))  # color4d = RED, GREEN, BLUE, OPACITY
 
         # Open file
         pctl.SetLayer(Edge_Cuts)
@@ -162,27 +163,27 @@ class GenFixture:
         pctl.ClosePlot()
 
         # Restore origin
-        self.brd.SetAuxOrigin (aux_origin_save)
+        self.brd.SetAuxOrigin(aux_origin_save)
 
     def Generate(self, path):
 
         # Get origin and board dimensions
-        self.GetOriginDimensions ()
+        self.GetOriginDimensions()
 
         # Get test points
-        self.GetTestPoints ()
+        self.GetTestPoints()
 
         # Plot DXF
-        self.PlotDXF (path)
+        self.PlotDXF(path)
 
         # Get revision
         if self.rev is None:
-            self.rev = "rev.%s" % self.brd.GetTitleBlock().GetRevision ()
+            self.rev = "rev.%s" % self.brd.GetTitleBlock().GetRevision()
             if self.rev == "":
                 self.rev = "rev.0"
 
         # Call openscad to generate fixture
-        args = "-D\'test_points=%s\'" % self.GetTestPointStr () 
+        args = "-D\'test_points=%s\'" % self.GetTestPointStr()
         args += " -D\'tp_min_y=%.02f\'" % self.min_y
         args += " -D\'mat_th=%.02f\'" % self.mat_th
         args += " -D\'pcb_th=%.02f\'" % self.pcb_th
@@ -196,67 +197,70 @@ class GenFixture:
         if self.rev != None:
             args += " -D\'rev=\"%s\"\'" % self.rev
         if self.washer_th != None:
-            args += " -D\'washer_th=%.02f\'" % float (self.washer_th)
+            args += " -D\'washer_th=%.02f\'" % float(self.washer_th)
         if self.nut_f2f != None:
-            args += " -D\'nut_od_f2f=%.02f\'" % float (self.nut_f2f)
+            args += " -D\'nut_od_f2f=%.02f\'" % float(self.nut_f2f)
         if self.nut_c2c != None:
-            args += " -D\'nut_od_c2c=%.02f\'" % float (self.nut_c2c)
+            args += " -D\'nut_od_c2c=%.02f\'" % float(self.nut_c2c)
         if self.nut_th != None:
-            args += " -D\'nut_th=%.02f\'" % float (self.nut_th)
+            args += " -D\'nut_th=%.02f\'" % float(self.nut_th)
         if self.pivot_d != None:
-            args += " -D\'pivot_d=%.02f\'" % float (self.pivot_d)
+            args += " -D\'pivot_d=%.02f\'" % float(self.pivot_d)
         if self.border != None:
-            args += " -D\'border=%.02f\'" % float (self.border)
+            args += " -D\'border=%.02f\'" % float(self.border)
 
         # Create output file name
         dxfout = path + "/" + self.prj_name + "-fixture.dxf"
         pngout = path + "/" + self.prj_name + "-fixture.png"
-        testout= path + "/" + self.prj_name + "-test.dxf"
+        testout = path + "/" + self.prj_name + "-test.dxf"
 
         # This will take a while, print something
-        print ("Generating Fixture...")
-        
+        print("Generating Fixture...")
+
         # Create test part
-        os.system ("openscad %s -D\'mode=\"testcut\"\' -o %s openfixture.scad" % (args, testout))
-        
+        print("openscad {} -D\'mode=\"testcut\"\' -o {} openfixture.scad".format(args, testout))
+        os.system("openscad %s -D\'mode=\"testcut\"\' -o %s openfixture.scad" % (args, testout))
+
         # Create rendering
-        os.system ("openscad %s -D\'mode=\"3dmodel\"\' --render -o %s openfixture.scad" % (args, pngout))
-      
+        print("openscad {} -D\'mode=\"3dmodel\"\' --render -o {} openfixture.scad".format(args, pngout))
+        os.system("openscad %s -D\'mode=\"3dmodel\"\' --render -o %s openfixture.scad" % (args, pngout))
+
         # Create laser cuttable fixture
-        os.system ("openscad %s -D\'mode=\"lasercut\"\' -o %s openfixture.scad" % (args, dxfout))
+        print("openscad {} -D\'mode=\"lasercut\"\' -o {} openfixture.scad".format(args,dxfout))
+        os.system("openscad %s -D\'mode=\"lasercut\"\' -o %s openfixture.scad" % (args, dxfout))
 
         # Print output
-        print ("Fixture generated: %s" % dxfout)
+        print("Fixture generated: %s" % dxfout)
 
-    def GetTestPointStr (self):
+    def GetTestPointStr(self):
         tps = "["
         for tp in self.test_points:
             tps += "[%.02f,%.02f]," % (tp[0], tp[1])
         return (tps + "]")
 
-    def GetTestPoints (self):
+    def GetTestPoints(self):
 
         # Iterate over all pads
-        for m in self.brd.GetModules ():
+        for m in self.brd.GetModules():
 
             # Iterate over all pads
-            for p in m.Pads ():
+            for p in m.Pads():
 
                 # Check that there is no paste and it's on front copper layer
-                if (p.IsOnLayer (self.layer) == True):
+                if (p.IsOnLayer(self.layer) == True):
 
                     # Are we forcing this pad?
-                    if (p.IsOnLayer (self.force_layer) == True):
+                    if (p.IsOnLayer(self.force_layer) == True):
                         pass
-                    
-                    #else check ignore cases
-                    elif ((p.IsOnLayer (self.ignore_layer) == True) or
-                          (p.IsOnLayer (self.paste) == True) or
-                          (p.GetAttribute () != PAD_ATTRIB_SMD)):
+
+                    # else check ignore cases
+                    elif ((p.IsOnLayer(self.ignore_layer) == True) or
+                          (p.IsOnLayer(self.paste) == True) or
+                          (p.GetAttribute() != PAD_ATTRIB_SMD)):
                         continue
-                                             
+
                     # Print position
-                    tp = ToMM (p.GetPosition ())
+                    tp = ToMM(p.GetPosition())
 
                     # Round x and y, invert x if mirrored
                     if self.mirror is False:
@@ -264,15 +268,15 @@ class GenFixture:
                     else:
                         x = self.dims[0] - (self.Round(tp[0] - self.origin[0]))
                     y = self.Round(tp[1] - self.origin[1])
-                        #print "tp = (%f, %f)" % (x,y)
-                    
+                    # print "tp = (%f, %f)" % (x,y)
+
                     # Check if less than min
                     if y < self.min_y:
                         self.min_y = y
-                    
+
                     # Save coordinates of pad
-                    self.test_points.append ([x, y])
-    
+                    self.test_points.append([x, y])
+
     def GetOriginDimensions(self):
         if (self.brd is None):
             return None
@@ -282,24 +286,24 @@ class GenFixture:
         max_y = 0
 
         # Get all drawings
-        for line in self.brd.GetDrawings ():
+        for line in self.brd.GetDrawings():
 
             # Check that it's in the outline layer
-            if line.GetLayerName () == 'Edge.Cuts':
+            if line.GetLayerName() == 'Edge.Cuts':
 
                 # Get bounding box
-                bb = line.GetBoundingBox ()
-                x = ToMM (bb.GetX ())
-                y = ToMM (bb.GetY ())
+                bb = line.GetBoundingBox()
+                x = ToMM(bb.GetX())
+                y = ToMM(bb.GetY())
 
                 # Debug
-                #print "(%f, %f)" % (x, y)
-                
+                # print "(%f, %f)" % (x, y)
+
                 # Min x/y will be origin
                 if x < self.origin[0]:
-                    self.origin[0] = self.Round (x)
+                    self.origin[0] = self.Round(x)
                 if y < self.origin[1]:
-                    self.origin[1] = self.Round (y)
+                    self.origin[1] = self.Round(y)
 
                 # Max x.y will be dimensions
                 if x > max_x:
@@ -308,77 +312,78 @@ class GenFixture:
                     max_y = y
 
         # Calculate dimensions
-        self.dims[0] = self.Round (max_x - self.origin[0])
-        self.dims[1] = self.Round (max_y - self.origin[1])
-        
+        self.dims[0] = self.Round(max_x - self.origin[0])
+        self.dims[1] = self.Round(max_y - self.origin[1])
+
+
 if __name__ == '__main__':
 
     # Create parser
-    parser = argparse.ArgumentParser ()
+    parser = argparse.ArgumentParser()
 
     # Add required arguments
-    parser.add_argument ('--board', help='<board_name.kicad_pcb>', required=True)
-    parser.add_argument ('--mat_th', help='material thickness (mm)', required=True)
-    parser.add_argument ('--out', help='output directory', required=True)
-    
+    parser.add_argument('--board', help='<board_name.kicad_pcb>', required=True)
+    parser.add_argument('--mat_th', help='material thickness (mm)', required=True)
+    parser.add_argument('--out', help='output directory', required=True)
+
     # Add optional arguments
-    parser.add_argument ('--pcb_th', help='pcb thickness (mm)')
-    parser.add_argument ('--screw_len', help='Assembly screw thread length (default = 16mm)')
-    parser.add_argument ('--screw_d', help='Assembly screw diameter (default=3mm)')
-    parser.add_argument ('--layer', help='F.Cu | B.Cu')
-    parser.add_argument ('--flayer', help='Eco1.User | Eco2.User')
-    parser.add_argument ('--ilayer', help='Eco1.User | Eco2.User')
-    parser.add_argument ('--rev', help='Override revisiosn')
-    parser.add_argument ('--washer_th', help='Washer thickness for hinge')
-    parser.add_argument ('--nut_f2f', help='hex nut flat to flat (mm)')
-    parser.add_argument ('--nut_c2c', help='hex nut corner to corner (mm)')
-    parser.add_argument ('--nut_th', help='hex nut thickness (mm)')
-    parser.add_argument ('--pivot_d', help='Pivot diameter (mm)')
-    parser.add_argument ('--border', help='Board (ledge) under pcb (mm)')
+    parser.add_argument('--pcb_th', help='pcb thickness (mm)')
+    parser.add_argument('--screw_len', help='Assembly screw thread length (default = 16mm)')
+    parser.add_argument('--screw_d', help='Assembly screw diameter (default=3mm)')
+    parser.add_argument('--layer', help='F.Cu | B.Cu')
+    parser.add_argument('--flayer', help='Eco1.User | Eco2.User')
+    parser.add_argument('--ilayer', help='Eco1.User | Eco2.User')
+    parser.add_argument('--rev', help='Override revisiosn')
+    parser.add_argument('--washer_th', help='Washer thickness for hinge')
+    parser.add_argument('--nut_f2f', help='hex nut flat to flat (mm)')
+    parser.add_argument('--nut_c2c', help='hex nut corner to corner (mm)')
+    parser.add_argument('--nut_th', help='hex nut thickness (mm)')
+    parser.add_argument('--pivot_d', help='Pivot diameter (mm)')
+    parser.add_argument('--border', help='Board (ledge) under pcb (mm)')
 
     # Get args
-    args = parser.parse_args ()
+    args = parser.parse_args()
 
     # Convert path to absolute
-    out_dir = os.path.abspath (args.out)
+    out_dir = os.path.abspath(args.out)
 
     # If output directory doesn't exist create it
-    if not os.path.exists (out_dir):
-        os.makedirs (out_dir)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
     # Load up the board file
-    brd = LoadBoard (args.board)
+    brd = LoadBoard(args.board)
 
     # Extract project name
-    prj_name = os.path.splitext (os.path.basename (args.board))[0]
-    
+    prj_name = os.path.splitext(os.path.basename(args.board))[0]
+
     # Save internal parameters
-    layer = brd.GetLayerID (args.layer)
-    flayer = brd.GetLayerID (args.flayer)
-    ilayer = brd.GetLayerID (args.ilayer)
+    layer = brd.GetLayerID(args.layer)
+    flayer = brd.GetLayerID(args.flayer)
+    ilayer = brd.GetLayerID(args.ilayer)
 
     # Check for pcb thickness
     if args.pcb_th is None:
         args.pcb_th = "1.6"
 
     # Create a fixture generator
-    fixture = GenFixture (prj_name, brd, args.mat_th)
+    fixture = GenFixture(prj_name, brd, args.mat_th)
 
     # Set parameters
-    fixture.SetParams (args.pcb_th, args.screw_len, args.screw_d);
+    fixture.SetParams(args.pcb_th, args.screw_len, args.screw_d);
 
     # Setup layers
-    fixture.SetLayers (layer=layer, flayer=flayer, ilayer=ilayer)
+    fixture.SetLayers(layer=layer, flayer=flayer, ilayer=ilayer)
 
     # Set optional arguments
-    fixture.SetOptional (rev=args.rev,
-                         washer_th=args.washer_th,
-                         nut_f2f=args.nut_f2f,
-                         nut_c2c=args.nut_c2c,
-                         nut_th=args.nut_th,
-                         pivot_d=args.pivot_d,
-                         border=args.border)
+    fixture.SetOptional(rev=args.rev,
+                        washer_th=args.washer_th,
+                        nut_f2f=args.nut_f2f,
+                        nut_c2c=args.nut_c2c,
+                        nut_th=args.nut_th,
+                        pivot_d=args.pivot_d,
+                        border=args.border)
 
     # Generate fixture
-    fixture.Generate (out_dir)
+    fixture.Generate(out_dir)
     # print fixture
